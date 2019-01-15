733a2229c02394464b8b1c26993c020e758ac3e5
Daniel Lo Nigro <d@d.sb>
2019-01-14 18:26:18 -0800

    Only update nightly repo if there's actually some new packages

--- update-deb.sh
+++ update-deb.sh
@@ -6,5 +6,12 @@
 # aptly publish repo -gpg-key=4F77679369475BAA -architectures=i386,amd64 yarn-nightly yarn-nightly
 
 set -ex
-aptly repo add -remove-files=true yarn-nightly ./nightly/deb-incoming/
-aptly publish update -gpg-key=4F77679369475BAA nightly yarn-nightly
+
+# Check if there's any .deb files to process
+files=$(shopt -s nullglob; echo ./nightly/deb-incoming/*.deb)
+if (( ${#files} )); then
+  aptly repo add -remove-files=true yarn-nightly ./nightly/deb-incoming/
+  aptly publish update -gpg-key=4F77679369475BAA nightly yarn-nightly
+else
+  echo 'No packages to process'
+fi
